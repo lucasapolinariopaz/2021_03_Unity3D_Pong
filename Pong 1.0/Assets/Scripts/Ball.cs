15418bf913856d9335f6ab83ca6a58b74f9fbbb3
Lucas Apolinário Paz <43834009+lucasapolinariopaz@users.noreply.github.com>
2021-03-25 23:37:42 -0300

    Objects created
    Object Paddle and Ball add to project

---
+++ Pong 1.0/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField]
+    private float m_Speed;
+    [SerializeField]
+    private float m_MaxSpeed;
+    [SerializeField]
+    private float m_SpeedIncBy;
+
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector2 m_Velocity;
+
+    private float m_InitialSpeed;
+    private Vector3 m_InitialPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
