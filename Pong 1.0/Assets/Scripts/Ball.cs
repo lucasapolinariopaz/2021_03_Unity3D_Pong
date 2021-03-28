d82c1aef7118f5ac07380427d75e70d49a34c000
Lucas Apolinário Paz <43834009+lucasapolinariopaz@users.noreply.github.com>
2021-03-28 16:46:43 -0300

    mechanic of ball's collision
    added code for detection of ball's collision between paddle's or walls of the game

--- Pong 1.0/Assets/Scripts/Ball.cs
+++ Pong 1.0/Assets/Scripts/Ball.cs
@@ -36,4 +36,62 @@ public class Ball : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        switch(other.gameObject.name)
+        {
+            case "PaddleP1":
+                IncSpeed();
+                UpdateVelocity(1.0f, GetHitYAxis(other));
+                break;
+            case "PaddleP2":
+                IncSpeed();
+                UpdateVelocity(-1.0f, GetHitYAxis(other));
+                break;
+            case "WallTop":
+            case "WallBottom":
+                UpdateVelocity(m_Velocity.x, -m_Velocity.y);
+                break;
+            case "WallLeft":
+                ResetSpeed();
+                UpdateVelocity(1.0f, 0.0f);
+                transform.position = m_InitialPosition;
+                break;
+            case "WallRight":
+                ResetSpeed();
+                UpdateVelocity(-1.0f, 0.0f);
+                transform.position = m_InitialPosition;
+                break;
+        }
+    }
+
+    private float GetHitYAxis(Collision2D paddle)
+    {
+        return (transform.position.y - paddle.transform.position.y) / paddle.collider.bounds.size.y;
+    }
+
+    private void ResetSpeed()
+    {
+        m_Speed = m_InitialSpeed;
+    }
+
+    private void IncSpeed()
+    {
+        m_Speed += m_SpeedIncBy;
+
+        if(m_Speed > m_MaxSpeed)
+        {
+            m_Speed = m_MaxSpeed;
+        }
+    }
+
+    private void UpdateVelocity(float x, float y)
+    {
+        m_Velocity.x = x;
+        m_Velocity.y = y;
+        m_Velocity = m_Velocity.normalized * m_Speed;
+
+        m_Rigidbody2D.velocity = m_Velocity;
+    }
 }
