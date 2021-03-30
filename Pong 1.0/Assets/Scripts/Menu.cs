b919e275da9ab599c7294f5d81a6c5af72f7fb06
Lucas Apolinário Paz <43834009+lucasapolinariopaz@users.noreply.github.com>
2021-03-29 23:09:46 -0300

    Initial Screen
    Created Initial Screen and added in build of the game

---
+++ Pong 1.0/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if(Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+	}
+}
